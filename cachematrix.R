65fa9fc4a449a54acd0461d506790a541aa58747
Pedro Centeno <pedro.jvrc.costa@gmail.com>
2015-02-22 21:40:08 +0000

    Programming Assignment 2 implementation code added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function generates a list whose components are four functions/ methods that will be
+## associated to a matrix that it receives as parameter. The functions implement the operations for 
+## defining and fetching the matrix value, and defining and fetching its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        x_inverse <- NULL
+        
+        set <- function( p_matrix ) {
+                x <<- p_matrix
+                x_inverse <- NULL       ##when matrix is defined its inverse is not yet available 
+        }
+        
+        get <- function() x
+        
+        setInverse <- function( p_inverse ) x_inverse <<- p_inverse
+        
+        getInverse <- function() x_inverse
+        
+        list( set = set,
+              get = get,
+              setInverse = setInverse,
+              getInverse = getInverse )
+        
 }
 
 
-## Write a short comment describing this function
+
+## The function returns the inverse of a 'cacheable' matrix passed to it as parameter 
+## If the inverse of that matrix has already been calculated, then it is stored (cached)
+## and so is fetched, without any further effort, and returned
+## Otherwise, the inverse not yet calculated, is calculated, stored, and returned
 
 cacheSolve <- function(x, ...) {
+        
         ## Return a matrix that is the inverse of 'x'
+        ## Matrix is assumed to be invertible
+        
+        x_inverse <- x$getInverse()
+        
+        if( ! is.null(x_inverse) ) {
+                return( x_inverse )
+        }
+        
+        a_matrix <- x$get
+        
+        x_inverse <- solve( x )
+        
+        x$setInverse( x_inverse )
+        
+        x_inverse
 }
+
